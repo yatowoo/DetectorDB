12a31e2024745f5ee0321bd6ddc42e146232a772
Yitao Wu <torrence@mail.ustc.edu.cn>
2018-03-24 20:41:36 +0800

    ADD url param for getFIle.php

--- getFile.php
+++ getFile.php
@@ -31,6 +31,35 @@ function download($file){
 	return $status;
 }
 
-download("/home/yato/Downloads/ct.root");
+function check_param($opt){
+	if(isset($_GET[$opt]))//判断所需要的参数是否存在，isset用来检测变量是true or false 
+	{ 
+	  // Verify input param - only num, char and '_'
+	  if(preg_match("/^[A-Za-z0-9_]+$/u", $_GET[$opt])){
+		  return true;
+	  }
+	}
+	return false;
+}
+
+// GET param : exp=EPD&type=FEE&uid=0
+$DATA_DIR="/home/yato/DATA";
+
+if(is_array($_GET)&&count($_GET)>0)//判断是否有Get参数 
+{ 
+	if(check_param('exp') && check_param('type') && check_param('uid')){
+
+	}
+	else{
+		echo 'Invalid param';
+		exit;
+	}
+}
+else{
+  echo 'Empty param';
+  exit;
+}
+
+download($DATA_DIR . "/" . $_GET["exp"] . "/" . $_GET["type"] . $_GET["uid"] . ".root");
 
 ?>
