5a8fd15906039ea77a662df4ff766547ef90015c
Yitao Wu <torrence@mail.ustc.edu.cn>
2018-03-24 23:09:37 +0800

    ADD detailView for EPD tables

--- load.js
+++ load.js
@@ -14,12 +14,70 @@ $(document).ready(function () {
     loadData("query.php");
 });
 
+function buildJsRootBtn(text, file, layout, action) {
+    var uri = '../jsroot?file=' + file + '&layout=' + layout + "&load=" + action;
+    return "<p><a class=\"btn btn-info\" href=\"" + encodeURI(uri) + "\"><b>" + text + "</b> </a></p>";
+}
+
+function buildList(header, data){
+    html = [];
+    html.push("<p>" + header + "</p>");
+    $.each(data,function(k,v){
+        html.push("<p><b>"+k+"</b>: "+v+"</p>");
+    });   
+    return html.join('');
+}
+
 function detailFormatter(index, row) {
     var html = [];
-    if (row['crtest'])
-        html.push("<p><a class=\"btn btn-info\" href=\"jsroot/?file=../result.root&layout=grid4x4&load=../drawTest.js\"><b>Cosmic-ray Test</b> </a></p>");
-    if (row['beamtest'])
-        html.push("<p><a class=\"btn btn-info\" href=\"jsroot/?file=../result.root&layout=grid4x4&load=../drawTest.js\"><b>Beam Test</b> </a></p>");
+    if (tbname == "mrpc") {
+        // Button - link to jsroot
+        if (row['crtest'])
+            html.push(
+                buildJsRootBtn("Cosmic-ray Test", "../result.root", "grid4x4", "../detdb/drawTest.js"));
+        if (row['beamtest'])
+            html.push(
+                buildJsRootBtn("Beam Test", "../result.root", "grid4x4", "../detdb/drawTest.js"));
+    } else if (tbname == 'epd-fee') {
+        // Button - link to jsroot
+        html.push(
+            buildJsRootBtn("Result", '"../detdb/getFile.php?exp=EPD&type=FEE&uid=' + row['uid'] + '"', "simple", "../detdb/drawTest.js")
+        );
+        // Table - pedestal
+        html.push("<table class='table'>");
+        html.push("<thead><tr><th>Pedestal</th>");
+        for (var i = 0; i < 16; i++) {
+            html.push("<th>CH-" + i + "</th>");
+        }
+        html.push("</tr></thead>");
+        html.push("<tbody>");
+        $.each(row["pedestal"], function (key, value) {
+            html.push("<tr><td>" + key + "</td>");
+            if ($.isArray(value)) {
+                $.each(value, function (k, v) {
+                    html.push("<td>" + v + "</td>");
+                });
+                html.push("</tr>");
+            }
+        });
+        html.push("</tbody>");
+        html.push("</table>");
+    }
+    else if(tbname == 'epd-rxb'){
+        // Button - link to jsroot
+        html.push(
+            buildJsRootBtn("Result", '"../detdb/getFile.php?exp=EPD&type=RXB&uid=' + row['uid'] + '"', "simple", "../detdb/drawTest.js")
+        );
+        // List
+        html.push(buildList("Current / A", row['current']));    
+    }else if(tbname == 'epd-sipm'){
+        // Button - link to jsroot
+        html.push(
+            buildJsRootBtn("Result", '"../detdb/getFile.php?exp=EPD&type=SiPM&uid=' + row['uid'] + '"', "simple", "../detdb/drawTest.js")
+        );
+        // List
+        html.push(buildList("Height / mm", row['vis-test']));   
+    }
     return html.join('');
 }
 
