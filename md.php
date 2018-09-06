2be6cf8781ce6a70c91c3ba0533b964ea1c01da6
yato <torrence@mail.ustc.edu.cn>
2018-09-06 22:57:04 +0800

    ADD null check

--- md.php
+++ md.php
@@ -51,58 +51,50 @@
 				tbmain.getElementsByTagName("td")[9].innerText = data.main.qa;
 				tbmain.getElementsByTagName("td")[11].innerText = data.main.test;
 				
-if(data.track != undefined){
-				tbmain.getElementsByTagName("td")[15].innerText = data.track[data.track.length-1].status;
-				tbmain.getElementsByTagName("td")[17].innerText = data.track[data.track.length-1].location;
-				tbmain.getElementsByTagName("td")[19].innerText = data.track[data.track.length-1].owner;
-				tbmain.getElementsByTagName("td")[21].innerText = data.track[data.track.length-1].date;
-				tbmain.getElementsByTagName("td")[23].innerText = data.track[data.track.length-1].comment;
+				if(data.track != undefined){
+					tbmain.getElementsByTagName("td")[15].innerText = data.track[data.track.length-1].status;
+					tbmain.getElementsByTagName("td")[17].innerText = data.track[data.track.length-1].location;
+					tbmain.getElementsByTagName("td")[19].innerText = data.track[data.track.length-1].owner;
+					tbmain.getElementsByTagName("td")[21].innerText = data.track[data.track.length-1].date;
+					tbmain.getElementsByTagName("td")[23].innerText = data.track[data.track.length-1].comment;
 				}
 				// Production QA
-				
-if(data.qa != undefined)
-$("#qa-chart")[0].src = data.qa.chart;
+				if(data.qa != undefined)
+					$("#qa-chart")[0].src = data.qa.chart;
 
 				// Test
 					// Setup
 				tbsetup = $(".markdown-body table")[1].getElementsByTagName("td");
-if(data.setup != undefined){
-				tbsetup[1].innerText = data.setup.from;
-				tbsetup[3].innerText = data.setup.to;
-				tbsetup[5].innerText = data.setup.location;
-				tbsetup[7].innerText = data.setup.sys;
-				tbsetup[9].innerText = data.setup.trigger;
-				tbsetup[11].innerText = data.setup.fee;
-				tbsetup[13].innerText = data.setup.daq;
-				tbsetup[15].innerText = data.setup.geometry;
-				tbsetup[17].innerText = data.setup.acceptance;
-if(data.setup.gas != undefined){
-				tbsetup[21].innerText = data.setup.gas["R134a"];
-				tbsetup[23].innerText = data.setup.gas["iso-C4H10"];
-				tbsetup[25].innerText = data.setup.gas["SF6"];
-}
-if(data.setup.env != undefined){
-				tbsetup[29].innerText = data.setup.env.T;
-				tbsetup[31].innerText = data.setup.env.RH;
-}
-}
+				if(data.setup != undefined){
+					tbsetup[1].innerText = data.setup.from;
+					tbsetup[3].innerText = data.setup.to;
+					tbsetup[5].innerText = data.setup.location;
+					tbsetup[7].innerText = data.setup.sys;
+					tbsetup[9].innerText = data.setup.trigger;
+					tbsetup[11].innerText = data.setup.fee;
+					tbsetup[13].innerText = data.setup.daq;
+					tbsetup[15].innerText = data.setup.geometry;
+					tbsetup[17].innerText = data.setup.acceptance;
+					if(data.setup.gas != undefined && data.setup.gas != null){
+						tbsetup[21].innerText = data.setup.gas["R134a"];
+						tbsetup[23].innerText = data.setup.gas["iso-C4H10"];
+						tbsetup[25].innerText = data.setup.gas["SF6"];
+					}
+					if(data.setup.env != undefined && data.setup.env != null){
+						tbsetup[29].innerText = data.setup.env.T;
+						tbsetup[31].innerText = data.setup.env.RH;
+					}
+				}
 					// Result
 				tbresult = $(".markdown-body table")[2].getElementsByTagName("td");
-if(data.test != undefined){
-				tbsetup[21].innerText = data.setup.gas["R134a"];
-				tbsetup[23].innerText = data.setup.gas["iso-C4H10"];
-				tbsetup[25].innerText = data.setup.gas["SF6"];
-				tbsetup[29].innerText = data.setup.env.T;
-				tbsetup[31].innerText = data.setup.env.RH;
-					// Result
-				tbresult = $(".markdown-body table")[2].getElementsByTagName("td");
-				tbresult[6].innerText = data.test.noiserate;
-				tbresult[7].innerText = data.test.efficiency;
-				tbresult[8].innerText = data.test.resolution;
-				tbresult[9].innerText = data.test.current;
-				// TODO : design API for PATH to binary object 
-				$("#jsroot-result")[0].href = data.test.rootfile;
-}
+				if(data.test != undefined){
+					tbresult[6].innerText = data.test.noiserate;
+					tbresult[7].innerText = data.test.efficiency;
+					tbresult[8].innerText = data.test.resolution;
+					tbresult[9].innerText = data.test.current;
+					// TODO : design API for PATH to binary object 
+					$("#jsroot-result")[0].href = data.test.rootfile;
+				}
 			});
 		});
 	</script>
